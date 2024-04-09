e75c953ec5e40ab7dd71ba048598a33d40c3b36c
Nguyen181124 <nguyen181124@gmail.com>
2024-04-09 13:47:44 +0700

    first commit

---
+++ inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Unitop Store</title>
+
+    </head>
+    <body>
+        <div id="site">
+            <div id="container">
+                <div id="header-wp" class="clearfix">
+                    <div class="wp-inner">
+                        <a href="?page=home" title="" id="logo" class="fl-left">UNITOP STORE</a>
+                        <div id="btn-respon" class="fl-right"><i class="fa fa-bars" aria-hidden="true"></i></div>
+                        <div id="cart-wp" class="fl-right">
+                            <a href="?page=cart" title="" id="btn-cart">
+                                <span id="icon"><img src="public/images/icon-cart.png" alt=""></span>
+                                <span id="num">5</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
